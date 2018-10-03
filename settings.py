1b5134a3478a40f8eeefd12b9a02e2fb8f7383f8
x4xGitHub <43009430+x4xGitHub@users.noreply.github.com>
2018-10-03 22:58:00 +0800

    Add files via upload

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings():
         self.ship_limit = 3
 
         # 子弹的设置
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 0, 0, 0
         self.bullets_allowed = 3
